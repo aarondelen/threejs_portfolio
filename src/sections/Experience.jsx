637a8d4531001c17e8f5c4cf664690ee6feee472
aarondelen <aarondelen.ad@gmail.com>
2025-09-04 23:56:59 +0800

    Im pissed so im committing this buklshtui ahhahaha fucking hell man

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { Suspense, useState } from 'react'
 import { Canvas } from '@react-three/fiber'
-
+import { workExperiences } from '../constants/index.js'
+import { div } from 'three/tsl'
+import CanvasLoader from '../components/CanvasLoader.jsx'
+import Developer from '../components/Developer.jsx'
+import { OrbitControls } from '@react-three/drei'
 
 const Experience = () => {
+    const [animationName, setAnimationName] = useState('salute');
+
+
   return (
     <section className='c-space my-20'>
         <div className='w-full text-white-600'>
@@ -11,13 +18,36 @@ const Experience = () => {
             <div className='work-container'>
                 <div className='work-canvas'>
                     <Canvas>
+                        <ambientLight intensity={7}/>
+                        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1}/>
+                        <directionalLight position={[10, 10, 10]} intensity={1}/>
 
+                        <OrbitControls enableZoom={false} maxPolarAngle={Math.PI / 2}/>
+                        <Suspense fallback={<CanvasLoader/>}>
+                            <Developer position-y={-3} scale={3} animationName={animationName}/>
+                        </Suspense>
                     </Canvas>
                 </div>
 
                 <div className='work-content'>
                     <div className='sm:py-10 py-5 sm:px-5 px-2.5'>
+                        {workExperiences.map(({id, name, pos, duration, title, icon, animation}) => (
+                            <div key={id} className='work-content_container group' onClick={() => setAnimationName(animation.toLowerCase())} onPointerOver={() => setAnimationName(animation.toLowerCase())} onPointerOut={() => setAnimationName('salute')}>
+                                <div className='flex flex-col h-full justify-start items-center py-2'>
+                                    <div className='work-content_logo'>
+                                        <img src={icon} alt="logo" className='w-full h-full' />
+                                    </div>
+                                    <div className='work-content_bar'></div>
+                                </div>
+                                <div className='sm:p-5 px-2.5 py-5'>
+                                    <p className='font-bold text-white-800'>{name}</p>
+                                    <p className='text-sm mb-5'>{pos} -- {duration}</p>
+                                    <p className='group-hover:text-white transition-all ease duration-150'>{title}</p>
+                                </div>
+                            </div>
 
+                            // https://models.readyplayer.me/68b961fd19533c93d0241f1e.glb
+                        ))}
                     </div>
                 </div>
             </div>
